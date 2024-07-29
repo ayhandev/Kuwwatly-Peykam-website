213be902b3c48f135a7b5756afd99cc0b7f9f5d5
Ayhan <jumanyyzowayhan32@gmail.com>
2024-07-29 18:49:33 +0500

    1.0.3

---
+++ static/loader.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    const content = document.getElementById('content');
+    
+    setTimeout(() => {
+        loader.style.opacity = '0'; // Начать анимацию исчезновения
+        setTimeout(() => {
+            loader.style.display = 'none'; // Полностью скрыть после окончания анимации
+            content.style.display = 'block';
+        }, 500); // Время должно совпадать с длительностью анимации исчезновения
+    }, 1000); // 3000 milliseconds = 3 seconds
+});
